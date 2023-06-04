9fbfe89997cdee65ec78e6aab2ef95f09f609c0e
Lee Mungai <Lee.mungai@gmail.com>
2023-06-04 14:22:00 +0300

    Add Qualifier classes and game properties, modify gameconfig class

--- src/main/java/com/prodedge/numberguess/config/GameConfig.java
+++ src/main/java/com/prodedge/numberguess/config/GameConfig.java
@@ -1,4 +1,43 @@
 package com.prodedge.numberguess.config;
 
+import com.prodedge.numberguess.classes.GuessCount;
+import com.prodedge.numberguess.classes.MaxNumber;
+import com.prodedge.numberguess.classes.MinNumber;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ComponentScan(basePackages = "com.prodedge")
+@PropertySource("classpath:application.properties")
 public class GameConfig {
+    // == fields ==
+    @Value("${game.maxNumber}")
+    private int maxNumber;
+
+    @Value("${game.minNumber}")
+    private int minNumber;
+
+    @Value("${game.guessCount}")
+    private int guessCount;
+
+    @Bean
+    @MaxNumber
+    public int maxNumber() {
+        return maxNumber;
+    }
+
+    @Bean
+    @MinNumber
+    public int minNumber() {
+        return minNumber;
+    }
+
+    @Bean
+    @GuessCount
+    public int guessCount() {
+        return guessCount;
+    }
 }
